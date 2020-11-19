65ce489467752cb30b08c3cc20d05493ac5da115
masonloch <masonloch@LCHs-MacBook-Air.local>
2020-11-19 14:00:04 +0800

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+let a=Math.floor(Math.random()*3)
+switch (a){
+    case 0:
+        console.log('try again next time')
+        break;
+    case 1:
+        console.log('You get one bonus');
+        break;
+    case 2:
+        console.log('You get 2 bonus');
+        break;
+    default:
+        console.log('try again next time!!!');
+}
